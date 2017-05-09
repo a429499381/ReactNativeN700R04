6b73db0aa96076e990bc9a79f20e7e1d8dab1153
xutao <429499381@qq.com>
2017-05-09 23:05:44 +0800

    ListView 组件基本搞清楚了， 熟悉了渲染 数据的基本方法
         参考网址  http://www.jianshu.com/p/1293bb8ac969
    
         下一步可以请求数据渲染

--- app/creation/List.js
+++ app/creation/List.js
@@ -9,31 +9,95 @@ import {
   ListView,
   TouchableHighlight,
   Image,
+  Dimensions,
 } from 'react-native';
 
-let data = ['apple','pear','banana','orange','apple','pear','banana',
-  'orange','apple','pear','banana','orange','apple','pear','banana','orange',
-  'apple','pear','banana','orange','apple','pear','banana','orange'];
+import Icon from 'react-native-vector-icons/Ionicons'
+
+var width = Dimensions.get('window').width
+
+var data = {
+    "_id":"630000201408286582","thumb":"http://dummyimage.com/1200x600/bdade8)","viedeo":"xutao"
+  }
+
+
 
 export default class List extends Component {
-  constructor(){
-    super();
-    this.dataSource = new ListView.DataSource({
-      rowHasChanged:(row1,row2) => row1 !== row2
+  constructor(props) {
+    super(props)
+    var ds = new ListView.DataSource({
+      rowHasChanged:(r1, r2) => r1 !== r2,
     })
-  };
+    this.state = {
+      dataSource: ds,
+      data: ['h', 'e', 'l', 'l', '0']
+    }
+  }
+
+  _renderRow(rowData, rowId) {
+    return (
+      <Text>{rowData + ' ' + rowId}</Text>
+    )
+  }
 
   render() {
     return (
       <View>
         <ListView
-          dataSource={this.dataSource.cloneWithRows(data)}
-          renderRow={(rowData) =>
-            <View style={{flex:1,margin:10}}>
-              <Text style={{fontSize:25}}>{rowData}</Text>
-            </View>}
+          dataSource={this.state.dataSource.cloneWithRows(this.state.data)}
+          renderRow={(rowData,sectionId,rowId) => this._renderRow(rowData, rowId)}
+          showsVerticalScrollIndicator = {false}
         />
       </View>
-    );
+    )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5fcff'
+  },
+  header: {
+    paddingTop: 25,
+    paddingBottom: 12,
+    backgroundColor: '#ee735c',
+  },
+  headerTitle: {
+    color: '#fff',
+    fontSize: 16,
+    textAlign: 'center',
+    fontWeight: '600'
+  },
+
+ item: {
+
+ },
+ title: {
+
+ },
+ thumb: {
+
+ },
+ play: {
+
+ },
+ itemFooter: {
+
+ },
+ handleBox: {
+
+ },
+ up: {
+
+ },
+ handleText: {
+
+ },
+ handleBox: {
+
+ },
+ handleText: {
+
+ },
+})
